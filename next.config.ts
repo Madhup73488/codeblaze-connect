c4d784d0eb0e433dd5193b2d9b9199381ac902ed
Madhu P <madhup73488@gmail.com>
2025-07-01 23:11:21 +0530

    feat: update backend URL

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        destination: "https://codeblaze-web-backend.onrender.com/api/:path*",
       },
     ];
   },
