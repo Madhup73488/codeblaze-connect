eb985ae5026c6459f576d53a3565aed68ed4145d
Madhu P <madhup73488@gmail.com>
2025-07-21 01:04:04 +0530

    fix: fixed case sensitive issue

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
